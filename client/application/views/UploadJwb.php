041b94979f4d9b97ccaab6bbc1a2c92cab3bab1d
anasyawulandari <anasya.wulan@gmail.com>
2019-11-23 02:29:06 +0700

    sec commit

--- client/application/views/UploadJwb.php
+++ client/application/views/UploadJwb.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
 
     <!-- jQuery library -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
@@ -20,6 +21,48 @@
 
     <!-- <link rel="stylesheet" href="assets/css/UploadJwb.css"> -->
     <style>
+        body{
+            background-color:#F3F2F2;
+            background-attachment: fixed !important;
+            background-sizecover;
+            font-family:'Roboto',sans-serif;
+        }
+        .nav-pills{
+        width: 342px;
+        }
+        .nav-item{
+        width: 50%;
+        }
+        .nav .nav-link{
+        border-radius: 30px;
+        height: 56px;
+        margin-top: 40px;
+        }
+        .nav-pills .nav-link{
+        font-weight: bold;
+        text-align: center;
+        background: #343436;
+        color: #fff;
+        }
+        .nav-pills .nav-link.active{
+        background: #fff;
+        color: #000;
+        }
+        .new{
+            margin-left: 350px;
+        }
+        .user{
+        position: fixed;
+        background: #353537;
+        box-shadow: 0px 10px 35px rgba(0, 0, 0, 0.15);
+        border-radius: 30px;
+        width: 215px;
+        height: 45px;
+        float: right;
+        margin-left: 1060px;
+        margin-top: -55px;
+
+        }
       .button{
           width: 144px;
           height: 40px;
@@ -42,9 +85,10 @@
             height: 540px;
             box-shadow: 0px 10px 35px rgba(0, 0, 0, 0.15);
             border-radius: 30px;
+            margin-top:40px;
         }
         .box-1{
-          margin-top:-10px; 
+          margin-top:40px; 
         }
         table{
             margin-top : 50px;
@@ -97,88 +141,102 @@
             border-radius: 10px;
         }
         .kd:hover{
-            color:white;
+            color:black;
             cursor: pointer;
          }
          .mk:hover{
             cursor: pointer;
          }
+         .user:hover{
+             cursor: pointer;
+         }
          
   </style>
-
     <title>UploadJawaban</title>
 </head>
-<body background-color: #B3ADAD;
-    background-attachment: fixed !important;
-    background-size: cover; >
-    <!-- <div class="container"> -->
+<body>
+    <!-- Nav pills -->
+  <div class="new">
+        <ul class="nav nav-pills" role="tablist">
+            <li class="nav-item">
+            <a class="nav-link active" data-toggle="pill" href="#home">Praktikan</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" data-toggle="pill" href="#menu1">Asprak</a>
+            </li>
+        </ul>
+    </div>
+    <div class="alert container3 user text-white">
+            Anasya Wulandari
+            <span class="ml-3"><img src="assets/image/user.png" ></span>
+     </div>
     <div class="row">
-       <div class="box">
-        <div class="col align-self-center ">
-        <p>Form Upload Jawaban</p>
-        <font size="+2" ></font>
-        <br /><br />
-        <form action="#">
-        <!-- <form-container> -->
-        <table>
-            <tr>
-                <td id="indeks"><label for="praktikum"><b>Praktikum</b></label></td>
-                <td><label class="mk"><b>Pemrograman Berbasis Obyek</b></label></td>
-            </tr>
-            <tr>
-                <td id="indeks"><label for="modul"><b>Modul </b></label></td>
-                <td>
-                <select class="mk" name="Modul 10">
-                        <option>Modul 1</option>
-                        <option>Modul 2</option>
-                        <option>Modul 3</option>
-                        <option>Modul 4</option>
-                        <option>Modul 5</option>
-                        <option>Modul 6</option>
-                        <option>Modul 7</option>
-                        <option>Modul 8</option>
-                        <option>Modul 9</option>
-                        <option>Modul 10</option>
-                    </select>
-                </td>
-            </tr>
-            <tr>
-                <td id="indeks"><label for="kelas"><b>Kelas</b> </label></td>
-                <td><label class="mk"><b>IF-41-10</b></label></td>
-            </tr>
-            <tr>
-                <td id="indeks"><label for="JDWL"><b>Jadwal Praktikum</b></label></td>
-                <td><label class="mk"><b>09:30- 12:30</b></label></td>
-            </tr>
-            <tr>
-                <td id="indeks"><label for="Asprak"><b>Asprak </b></label></td>
-                <td><label class="mk"><b>Febiana Anistya</b></label></td>
-            </tr>
-            <tr>
-                <td id="indeks"><label for="tes"><b>Tes/Jurnal</b></label></td>
-                <td>
-                    <select class="mk" name="tes">
-                        <option>Tes Awal</option>
-                        <option>Jurnal</option>
-                        <option>Tes Akhir</option>
-                    </select>
-                </td>
-            </tr>
-            <tr>
-                <td id="indeks"><label class="kode"><b>Kode Autentifikasi</b> </label></td>
-                <td ><input class="kd"  type="text" name="kode"></td>
-            </tr>  
-            <tr>
-                <td id="indeks"><label for="kode"><b>NIM</b> </label></td>
-                <td><label class="mk"><b>1301174028</b></label></td>
-            </tr>      
-            <tr>
-                <td id="indeks"><label for="file"><b>Upload File </b></label></td>
-                <td><input class="mk"  type="file" name="file" id="file" accept="image/jpeg, image/gif"/></td>
-            </tr>      
-        </table> 
-        <button type="button" class="btn btn-primary rounded-pill " href="#">Upload Jawaban</button>  
-        </form>
+        <div class="box">
+            <div class="col align-self-center ">
+                <p>Form Upload Jawaban</p>
+            <font size="+2" ></font>
+                <br /><br />
+            <form action="#">
+            <!-- <form-container> -->
+                <table>
+                    <tr>
+                        <td id="indeks"><label for="praktikum"><b>Praktikum</b></label></td>
+                        <td><label class="mk"><b>Pemrograman Berbasis Obyek</b></label></td>
+                    </tr>
+                    <tr>
+                        <td id="indeks"><label for="modul"><b>Modul </b></label></td>
+                        <td>
+                        <select class="mk" name="Modul 10">
+                                <option>Modul 1</option>
+                                <option>Modul 2</option>
+                                <option>Modul 3</option>
+                                <option>Modul 4</option>
+                                <option>Modul 5</option>
+                                <option>Modul 6</option>
+                                <option>Modul 7</option>
+                                <option>Modul 8</option>
+                                <option>Modul 9</option>
+                                <option>Modul 10</option>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td id="indeks"><label for="kelas"><b>Kelas</b> </label></td>
+                        <td><label class="mk"><b>IF-41-10</b></label></td>
+                    </tr>
+                    <tr>
+                        <td id="indeks"><label for="JDWL"><b>Jadwal Praktikum</b></label></td>
+                        <td><label class="mk"><b>09:30- 12:30</b></label></td>
+                    </tr>
+                    <tr>
+                        <td id="indeks"><label for="Asprak"><b>Asprak </b></label></td>
+                        <td><label class="mk"><b>Febiana Anistya</b></label></td>
+                    </tr>
+                    <tr>
+                        <td id="indeks"><label for="tes"><b>Tes/Jurnal</b></label></td>
+                        <td>
+                            <select class="mk" name="tes">
+                                <option>Tes Awal</option>
+                                <option>Jurnal</option>
+                                <option>Tes Akhir</option>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td id="indeks"><label class="kode"><b>Kode Autentifikasi</b> </label></td>
+                        <td ><input class="kd"  type=" text" name=" kode"></td>
+                    </tr>  
+                    <tr>
+                        <td id="indeks"><label for="kode"><b>NIM</b> </label></td>
+                        <td><label class="mk"><b>1301174028</b></label></td>
+                    </tr>      
+                    <tr>
+                        <td id="indeks"><label for="file"><b>Upload File </b></label></td>
+                        <td><input class="mk"  type="file" name="file" id="file" accept="image/jpeg, image/gif"/></td>
+                    </tr>      
+                </table> 
+                <button type="button" class="btn btn-primary rounded-pill " href="#">Upload Jawaban</button>  
+            </form>
         </div>
       </div>
       <div class="box-1">
@@ -187,7 +245,7 @@
             </div>
         </div>
         </div>
-    <!-- </div> -->
+   
     
 </body>
 </html>
