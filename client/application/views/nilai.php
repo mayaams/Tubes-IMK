ff2a35079ca6f9a784eec2ea3a3257ffde512b08
Febiana Anistya <febiana.anistya@gmail.com>
2019-11-23 23:28:24 +0700

    nilai done

--- client/application/views/nilai.php
+++ client/application/views/nilai.php
@@ -2,10 +2,10 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Nilai</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Nilai</title>
 </head>
 
 <!-- Stylesheets -->
@@ -32,266 +32,243 @@
 
 
 <script type="text/javascript">
+  window.onload = function() {
 
-    window.onload = function() {
+    // Construct options first and then pass it as a parameter
+    var options1 = {
+      animationEnabled: true,
+      theme: "light2", // "light2", "dark1", "dark2"
+      data: [{
+        type: "column", //change it to line, area, bar, pie, etc
+        dataPoints: [{
+            label: "Modul 1",
+            y: 99
+          },
+          {
+            label: "Modul 2",
+            y: 80
+          },
+          {
+            label: "Modul 3",
+            y: 75
+          },
+          {
+            label: "Modul 4",
+            y: 80
+          },
+          {
+            label: "Modul 5",
+            y: 90
+          }
+        ]
+      }]
+    };
 
-        // Construct options first and then pass it as a parameter
-        var options1 = {
-            animationEnabled: true,
-            theme: "light2", // "light2", "dark1", "dark2"
-            data: [{
-                type: "column", //change it to line, area, bar, pie, etc
-                dataPoints: [{
-                        label: "Modul 1",
-                        y: 75
-                    },
-                    {
-                        label: "Modul 2",
-                        y: 99
-                    },
-                    {
-                        label: "Modul 3",
-                        y: 80
-                    }
-                ]
-            }]
-        };
+    $("#resizable").resizable({
+      create: function(event, ui) {
+        //Create chart.
+        $("#chartContainer1").CanvasJSChart(options1);
+      },
+      resize: function(event, ui) {
+        //Update chart size according to its container size.
+        $("#chartContainer1").CanvasJSChart().render();
+      }
+    });
 
-        $("#resizable").resizable({
-            create: function(event, ui) {
-                //Create chart.
-                $("#chartContainer1").CanvasJSChart(options1);
-            },
-            resize: function(event, ui) {
-                //Update chart size according to its container size.
-                $("#chartContainer1").CanvasJSChart().render();
-            }
-        });
-
-    }
+  }
 </script>
 
 <style media="screen">
-/* NAVBAR */
-.section {
-  min-height: 800px;
-  position: relative;
-  text-align: center;
-  font-family: sans-serif, arial;
-  margin: 0;
-}
+  /* NAVBAR */
+  .nav {
+    margin-left: 390px;
+    margin-top: 50px;
+    background: #353537;
+    box-shadow: 0px 10px 35px rgba(0, 0, 0, 0.15);
+    border-radius: 30px;
+    width: 257px;
+    height: 44px;
+    position: fixed;
+    z-index: 3;
+  }
 
-h1, p {
-  margin: 0;
-  font-family: sans-serif, arial;
-  font-weight: bold;
-}
+  .nav a {
+    padding: 13.7px 20px;
+    color: white;
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: 600;
+    border-radius: 30px;
+    font-size: 17px;
+    line-height: 2.6;
+    -webkit-transition: all 0.2s ease-out;
+    -moz-transition: all 0.2s ease-out;
+    -ms-transition: all 0.2s ease-out;
+    -o-transition: all 0.2s ease-out;
+    transition: all 0.2s ease-out;
+  }
 
-.text-wr {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  -webkit-transform: translate(-50%, -50%);
-  -moz-transform: translate(-50%, -50%);
-  -o-transform: translate(-50%, -50%);
-  -ms-transform: translate(-50%, -50%);
-  transform: translate(-50%, -50%);
-}
+  .nav a.active {
+    background-color: white;
+    box-shadow: 0px 10px 35px rgba(0, 0, 0, 0.1);
+    color: #353537 !important;
+  }
 
-#section1 {
-  background: #5c53aa;
-  color: #8d86c3;
-}
-
-#section2 {
-  background: #4bbfc3;
-  color: #81d2d5;
-}
-
-#section3 {
-  background: #283044;
-  color: #686e7c;
-}
-
-#section4 {
-  background: #ebf5ee;
-  color: #aeb9b9;
-}
-
-h1 {
-  font-size: 50px;
-  margin-bottom: 25px;
-}
-
-.title-top {
-  font-size: 60px;
-  padding-bottom: 30px;
-}
-
-.title-bottom {
-  font-size: 30px;
-}
-
-.title-tx {
-  font-size: 20px;
-  opacity: 0.8;
-}
-
-.nav {
-  margin-left: 40%;
-}
-
-.nav a {
-  padding: 7px 20px;
-  border-radius: 7px;
-  margin-right: 10px;
-  background: rgba(255, 255, 255, 0.5);
-  -webkit-transition: all 0.2s ease-out;
-  -moz-transition: all 0.2s ease-out;
-  -ms-transition: all 0.2s ease-out;
-  -o-transition: all 0.2s ease-out;
-  transition: all 0.2s ease-out;
-  text-decoration: none;
-  color: black;
-  font-family: sans-serif, Roboto;
-  font-weight: 100;
-}
-
-.nav a.active {
-  background: rgba(0, 0, 0, 0.5);
-  color: white;
-}
-
-.nav a:hover {
+  /* .nav a:hover {
   background: rgba(255, 255, 255, 0.7);
-}
-
-.fork-wr img {
-  position: fixed;
-  z-index: 99;
-  top: 0;
-  right: 0;
-  border: 0;
-}
-
+} */
 </style>
 
 <body>
 
-    <!-- ini side bar -->
-    <div id="sidebar">
-        <h2>IMPULSE</h2>
-        <ul>
-            <img id="iconsiderbar" src="<?php echo base_url('/assets/image/home.png'); ?>" style="margin-left: -20px;">
-            <li><a href="#"><b>Home</b></a></li>
-            <img id="iconsiderbar" src="<?php echo base_url('/assets/image/nilai.png'); ?>" style="margin-left: -20px;">
-            <li><b>Nilai</b></li>
-            <img id="iconsiderbar" src="<?php echo base_url('/assets/image/upload.png'); ?>" style="margin-left: -20px;">
-            <li><b>Upload Jawaban</b></li>
-            <img id="iconsiderbar" src="<?php echo base_url('/assets/image/jadwal.png'); ?>" style="margin-left: -20px;">
-            <li><b>Jadwal</b></li>
-            <img id="iconsiderbar" src="<?php echo base_url('/assets/image/data.png'); ?>" style="margin-left: -20px;"">
+  <!-- ini side bar -->
+  <div id="sidebar">
+    <h2>IMPULSE</h2>
+    <ul>
+      <img id="iconsiderbar" src="<?php echo base_url('/assets/image/home.png'); ?>" style="margin-left: -20px;">
+      <li><a href="#"><b>Home</b></a></li>
+      <img id="iconsiderbar" src="<?php echo base_url('/assets/image/nilai.png'); ?>" style="margin-left: -20px;">
+      <li><b>Nilai</b></li>
+      <img id="iconsiderbar" src="<?php echo base_url('/assets/image/upload.png'); ?>" style="margin-left: -20px;">
+      <li><b>Upload Jawaban</b></li>
+      <img id="iconsiderbar" src="<?php echo base_url('/assets/image/jadwal.png'); ?>" style="margin-left: -20px;">
+      <li><b>Jadwal</b></li>
+      <img id="iconsiderbar" src="<?php echo base_url('/assets/image/data.png'); ?>" style="margin-left: -20px;"">
           <li><b>Data IFLAB</b></li>
         </ul>
     </div>
 
     <!-- ini drop down -->
     <div class=" dropdown">
-            <button class="btn btn-secondary dropdown-toggle drpdwn" type="button" id="dropdownProfile" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="border-radius: 30px; height: 45px;width: 226px;">
-                Anasya Wulandari<span> <i class="fas fa-user"></i></span>
-            </button>
-            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                <a class="dropdown-item" href="#">Logout</a>
+      <button class="btn btn-secondary dropdown-toggle drpdwn" type="button" id="dropdownProfile" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="border-radius: 30px; height: 45px;width: 226px;">
+        Anasya Wulandari<span> <i class="fas fa-user"></i></span>
+      </button>
+      <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+        <a class="dropdown-item" href="#">Logout</a>
 
-            </div>
-    </div>
-
-    <!-- ini navbar -->
-    <div class="nav" style="margin-left: 400px; margin-top: 100px;">
-          <nav>
-              <a href="#conGrafikNilai">Grafik Nilai</a>
-              <a href="#conKeterangan">Keterangan</a>
-          </nav>
       </div>
+  </div>
 
-    <div class="dropdown" style="margin-top: 209px; margin-left: 470px">
-        <span><b style="font-size: 18px;">Praktikum</b></span>
-        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="">
-            Pemrograman Berorientasi Objek (PBO)
-        </button>
-        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-            <a class="dropdown-item" href="#">Pemrograman Berorientasi Objek (PBO)</a>
-            <a class="dropdown-item" href="#">Jaringan Komputer (JRK)</a>
-        </div>
+  <!-- ini navbar -->
+  <div class="nav">
+    <nav>
+      <a href="#bagianNilai">Grafik Nilai</a>
+      <a href="#conKeterangan">Keterangan</a>
+    </nav>
+  </div>
+
+  <div class="dropdown" id="bagianNilai" style="margin-left: 400px">
+    <br><br><br><br><br><br><br><br>
+    <span><b style="font-size: 18px;">Praktikum</b></span>
+    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="">
+      Pemrograman Berorientasi Objek (PBO)
+    </button>
+    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+      <a class="dropdown-item" href="#">Pemrograman Berorientasi Objek (PBO)</a>
+      <a class="dropdown-item" href="#">Jaringan Komputer (JRK)</a>
     </div>
+  </div>
 
 
 
-    <!-- ini bagian nilai -->
-    <div class="container" id="conGrafikNilai" style="margin-top: 30px;">
-        <br><br>
-        <h4 style="margin-left: 70px;"><b>Grafik Nilai</b></h4>
-        <br>
-        <div id="resizable" style="height: 300px; width: 900px; margin: auto">
-            <div id="chartContainer1" style="height: 100%; width: 100%;"></div>
-        </div>
+  <!-- ini bagian nilai -->
+  <div class="container" id="conGrafikNilai" style="margin-top: 40px;">
+    <br><br>
+    <h4 style="margin-left: 70px;"><b>Grafik Nilai</b></h4>
+    <br>
+    <div id="resizable" style="height: 300px; width: 900px; margin: auto">
+      <div id="chartContainer1" style="height: 100%; width: 100%;"></div>
     </div>
+  </div>
 
-    <div class="tab-content" id="conKeterangan">
-        <div class="container">
-            <br><br>
-            <h4 style="margin-left: 70px;"><b>Keterangan</b></h4>
-            <br>
-            <table class="table table-hover" id="tabelNilai" style="width: 90%; margin: auto; text-align: center; border-radius: 30px;">
-                <thead class="thead-dark">
-                    <tr>
-                        <th scope="col">Modul</th>
-                        <th scope="col">Asisten Praktikum</th>
-                        <th scope="col">Tanggal</th>
-                        <th scope="col">Tes Awal</th>
-                        <th scope="col">Jurnal</th>
-                        <th scope="col">Tes Akhir</th>
-                        <th scope="col">Skill</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Yayan Prima</td>
-                        <td>19/09/2019</td>
-                        <td>10</td>
-                        <td>60</td>
-                        <td>5</td>
-                        <td>0</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Yayan Prima</td>
-                        <td>26/09/2019</td>
-                        <td>10</td>
-                        <td>60</td>
-                        <td>5</td>
-                        <td>0</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td>Yayan Prima</td>
-                        <td>03/10/2019</td>
-                        <td>10</td>
-                        <td>60</td>
-                        <td>5</td>
-                        <td>0</td>
-                    </tr>
-                </tbody>
-            </table><br><br>
-        </div>
+  <div class="tab-content" id="conKeterangan">
+    <div class="container">
+      <br><br>
+      <h4 style="margin-left: 70px;"><b>Keterangan</b></h4>
+      <br>
+      <table class="table table-hover" id="tabelNilai" style="width: 90%; margin: auto; text-align: center; border-radius: 30px;">
+        <thead class="thead-dark">
+          <tr>
+            <th scope="col">Modul</th>
+            <th scope="col">Asisten Praktikum</th>
+            <th scope="col">Tanggal</th>
+            <th scope="col">Tes Awal</th>
+            <th scope="col">Jurnal</th>
+            <th scope="col">Tes Akhir</th>
+            <th scope="col">Skill</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Yayan Prima</td>
+            <td>19/09/2019</td>
+            <td>10</td>
+            <td>60</td>
+            <td>5</td>
+            <td>24</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Yayan Prima</td>
+            <td>26/09/2019</td>
+            <td>10</td>
+            <td>50</td>
+            <td>20</td>
+            <td>0</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>Yayan Prima</td>
+            <td>03/10/2019</td>
+            <td>10</td>
+            <td>55</td>
+            <td>10</td>
+            <td>0</td>
+          </tr>
+          <tr>
+            <th scope="row">4</th>
+            <td>Yayan Prima</td>
+            <td>03/10/2019</td>
+            <td>10</td>
+            <td>60</td>
+            <td>5</td>
+            <td>5</td>
+          </tr>
+          <tr>
+            <th scope="row">5</th>
+            <td>Yayan Prima</td>
+            <td>03/10/2019</td>
+            <td>10</td>
+            <td>60</td>
+            <td>20</td>
+            <td>0</td>
+          </tr>
+        </tbody>
+      </table><br><br>
     </div>
-    <script src="<?php echo base_url(); ?>/assets/js/slideNav.js" type="text/javascript"></script>
-    <script>
-	    window.slide = new SlideNav({
-		changeHash: true
-	    });
-    </script>
+  </div><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+
+  <div class="area">
+    <ul class="circles">
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
+  </div>
+  <script src="<?php echo base_url(); ?>/assets/js/slideNav.js" type="text/javascript"></script>
+  <script>
+    window.slide = new SlideNav({
+      changeHash: true
+    });
+  </script>
 
 </body>
 
