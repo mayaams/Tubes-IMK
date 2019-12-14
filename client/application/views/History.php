f64f827fced6cf7cf9f99fa235465e68b94577a9
anasyawulandari <anasya.wulan@gmail.com>
2019-12-14 21:07:23 +0700

    one more

--- client/application/views/History.php
+++ client/application/views/History.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
      <!-- Stylesheets -->
+     <link rel="stylesheet" href="/css/all.min">
      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
@@ -18,30 +19,183 @@
 
     <!-- Latest compiled JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="assets/css/history.css">    
+    <!-- <link rel="stylesheet" href="assets/css/history.css"> -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>/assets/css/upload.css">    
+    <link rel="stylesheet" href="<?php echo base_url(); ?>/assets/css/nilai.css">
+    <style>
+    body{
+        background-color:#F3F2F2;
+        background-attachment: fixed !important;
+        background-sizecover;
+        font-family:'Roboto',sans-serif;
+        }
+        .nav-pills{
+        width: 342px;
+        margin-left:-80px;
+        }
+        .nav-item{
+        width: 50%;
+        }
+        .nav .nav-link{
+        border-radius: 30px;
+        height: 46px;
+        margin-top: 60px;
+        }
+        .nav-pills .nav-link{
+        font-weight: bold;
+        text-align: center;
+        background: #343436;
+        color: #fff;
+        }
+        .nav-pills .nav-link.active{
+        background: #fff;
+        color: #000;
+        top:15px;
+        }
+        .new{
+            margin-left: 460px;
+        }
+        
+      .button{
+          width: 50px;
+          height: 60px;
+          border-radius: 15px;
+         
+        }
+        .nav {
+            margin-left: 400px;
+            margin-top: 50px;
+            background: #353537;
+            box-shadow: 0px 10px 35px rgba(0, 0, 0, 0.15);
+            border-radius: 30px;
+            width: 242px;
+            height: 37px;
+            position: absolute;
+            z-index: 3;
+        }
+        .nav a {
+            padding: 12px 10px;
+            color: white;
+            font-family: Roboto;
+            font-style: normal;
+            font-weight: 600;
+            border-radius: 30px;
+            font-size: 13px;
+            line-height: 2.9;
+            -webkit-transition: all 0.2s ease-out;
+            -moz-transition: all 0.2s ease-out;
+            -ms-transition: all 0.2s ease-out;
+            -o-transition: all 0.2s ease-out;
+            transition: all 0.2s ease-out;
+        }
+
+        .nav a.active {
+            background-color: white;
+            box-shadow: 0px 10px 35px rgba(0, 0, 0, 0.1);
+            color: #353537 !important;
+        }
+
+        .drpdwn{
+          background: #1e1e1e;
+          border-radius: 30px;
+          float:right;
+          font-family:'roboto',sans-serif;
+          margin-right:55px;
+          margin-top:-10px;
+          width: 100px;
+          height: 35px;
+          position: absolute;
+        }
+        .box{
+            background-color :  #fff;
+            width: 900px;
+            height: 600px;
+            box-shadow: 0px 10px 35px rgba(0, 0, 0, 0.15);
+            border-radius: 30px;
+            margin-top:70px;
+        }
+        .row{
+            margin-right:50px;
+            float:right;
+            margin-top:90px;
+
+        }
+        .mk{
+            margin-top:10px;
+            margin-left:10px;
+            font-family:'roboto',sans-serif;
+            background-color: #C4C4C4;
+            width: 270px;
+            height:30px;
+            border-radius: 30px;
+        }
+        option{
+            border-radius: 30px;
+        }
+        .box-1 img{
+          margin-top:60px;
+          width: 500px;
+          height: 400px;
+        }
+
+        
+    </style>
     <title>History Upload</title>
 </head>
 <body>
+    <!-- ini side bar -->
+    <div id="sidebar">
+      <h2>IMPULSE</h2>
+      <ul>
+        <img id="iconsiderbar" src="<?php echo base_url('/assets/image/home.png'); ?>" style="margin-left: -20px;">
+        <li><a href="#"><b>Home</b></a></li>
+        <img id="iconsiderbar" src="<?php echo base_url('/assets/image/nilai.png'); ?>" style="margin-left: -20px;">
+        <li><b>Nilai</b></li>
+        <img id="iconsiderbar" src="<?php echo base_url('/assets/image/upload.png'); ?>" style="margin-left: -20px;">
+        <li><b>Upload Jawaban</b></li>
+        <img id="iconsiderbar" src="<?php echo base_url('/assets/image/jadwal.png'); ?>" style="margin-left: -20px;">
+        <li><b>Jadwal</b></li>
+        <img id="iconsiderbar" src="<?php echo base_url('/assets/image/data.png'); ?>" style="margin-left: -20px;">
+            <li><b>Data IFLAB</b></li>
+          </ul>
+      </div>
+
+    <!-- Nav pills -->
 <div class="header">
-  <div class="new">
-        <ul class="nav nav-pills" role="tablist">
-            <li class="nav-item">
-            <a class="nav-link active" data-toggle="pill" href="#home">Upload Jawaban</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link" data-toggle="pill" href="#menu1">History Jawaban</a>
-            </li>
-        </ul>
+    <!-- ini navbar -->
+  <div class="nav">
+    <nav>
+      <a class="" href="#upload">Upload Jawaban</a>
+      <a class="active" href="#History" style="margin-left: 3.5px">History Jawaban</a>
+    </nav>
+  </div>
+  <!-- ini dropdown -->
+  <div class=" dropdown">
+    <button class="btn btn-secondary dropdown-toggle drpdwn" type="button" id="dropdownProfile" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="border-radius: 30px; height: 45px;width: 226px;">
+         Anasya Wulandari<span> <i class="fas fa-user"></i></span>
+    </button>
+    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+        <a class="dropdown-item" href="#">Profil</a>
+        <a class="dropdown-item" href="#">Logout</a>
     </div>
-    <div class="dropdown">
-            <button class="btn btn-secondary dropdown-toggle drpdwn" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Anasya Wulandari<i class="fas fa-user"></i>
-            </button>
-            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                <a class="dropdown-item" href="#">Profil</a>
-                <a class="dropdown-item" href="#">Logout</a>
+  </div>
+  <div class="row">
+        <div class="box">
+            <div class="col align-self-center ">
+                        <select class="mk" name="Matkul">
+                                <option>Pemrograman Berorientasi Obyek</option>
+                                <option>Jaringan Komputer</option>
+                        </select> 
             </div>
-    </div>
+            <div class="box-1" style="">
+            <div class="col align-self-center">
+            <img src="assets/image/history.png" alt="Avatar" class="image" >
+            </div>
+        </div>
+      </div>
+
 </div>
+            
+   
 </body>
 </html>
--- client/application/views/UploadJwb.php
+++ client/application/views/UploadJwb.php
@@ -10,6 +10,12 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
 
+    <!-- Stylesheets -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>/assets/css/nilai.css">
+    <link href="https://canvasjs.com/assets/css/jquery-ui.1.11.2.min.css" rel="stylesheet" />
+    <link href="<?= base_url() ?>assets/fontawesome/css/all.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="/css/all.min">
+
     <!-- jQuery library -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
@@ -54,27 +60,46 @@
         .new{
             margin-left: 460px;
         }
-        /* .header{
-        /* position: fixed; */
         
-        /* .user{
-        position: fixed;
-        background: #353537;
-        box-shadow: 0px 10px 35px rgba(0, 0, 0, 0.15);
-        border-radius: 30px;
-        width: 215px;
-        height: 45px;
-        float: right;
-        margin-left: 1500px;
-        margin-top: -40px;
-        } */
       .button{
           width: 50px;
           height: 60px;
           border-radius: 15px;
          
         }
+        .nav {
+            margin-left: 400px;
+            margin-top: 50px;
+            background: #353537;
+            box-shadow: 0px 10px 35px rgba(0, 0, 0, 0.15);
+            border-radius: 30px;
+            width: 242px;
+            height: 37px;
+            position: absolute;
+            z-index: 3;
+        }
+        .nav a {
+            padding: 12px 10px;
+            color: white;
+            font-family: Roboto;
+            font-style: normal;
+            font-weight: 600;
+            border-radius: 30px;
+            font-size: 13px;
+            line-height: 2.9;
+            -webkit-transition: all 0.2s ease-out;
+            -moz-transition: all 0.2s ease-out;
+            -ms-transition: all 0.2s ease-out;
+            -o-transition: all 0.2s ease-out;
+            transition: all 0.2s ease-out;
+        }
 
+        .nav a.active {
+            background-color: white;
+            box-shadow: 0px 10px 35px rgba(0, 0, 0, 0.1);
+            color: #353537 !important;
+        }
+        
         .btn-primary {
         width: 150px;
         height: 45px;
@@ -100,19 +125,20 @@
           margin-right: 100px;
           
         }
-        img{
+        /* img{
             width: 400px;
             height: 480px;
-        }
+        } */
         .drpdwn{
           background: #1e1e1e;
           border-radius: 30px;
           float:right;
           font-family:'roboto',sans-serif;
           margin-right:55px;
-          margin-top:-50px;
-          width: 200px;
-          height: 48px;
+          margin-top:-10px;
+          width: 100px;
+          height: 35px;
+          position: absolute;
         }
         
         table{
@@ -122,7 +148,7 @@
         .row{
             margin-right:-55px;
             float:right;
-            margin-top:50px;
+            margin-top:90px;
         }
         p{
             position: absolute;
@@ -183,37 +209,52 @@
          }
          label{
              font-size:15px;
-             margin-top:10px;
+             /* margin-top:-10px; */
          }
          #indeks{
-             margin-top:2px;
+             margin-top:30px;
          }
 
   </style>
     <title>UploadJawaban</title>
 </head>
 <body>
+     <!-- ini side bar -->
+     <div id="sidebar">
+      <h2>IMPULSE</h2>
+      <ul>
+        <img id="iconsiderbar" src="<?php echo base_url('/assets/image/home.png'); ?>" style="margin-left: -20px;">
+        <li><a href="#"><b>Home</b></a></li>
+        <img id="iconsiderbar" src="<?php echo base_url('/assets/image/nilai.png'); ?>" style="margin-left: -20px;">
+        <li><b>Nilai</b></li>
+        <img id="iconsiderbar" src="<?php echo base_url('/assets/image/upload.png'); ?>" style="margin-left: -20px;">
+        <li><b>Upload Jawaban</b></li>
+        <img id="iconsiderbar" src="<?php echo base_url('/assets/image/jadwal.png'); ?>" style="margin-left: -20px;">
+        <li><b>Jadwal</b></li>
+        <img id="iconsiderbar" src="<?php echo base_url('/assets/image/data.png'); ?>" style="margin-left: -20px;">
+            <li><b>Data IFLAB</b></li>
+          </ul>
+      </div>
+
     <!-- Nav pills -->
 <div class="header">
-  <div class="new">
-        <ul class="nav nav-pills" role="tablist">
-            <li class="nav-item">
-            <a class="nav-link active" data-toggle="pill" href="#home">Upload Jawaban</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link" data-toggle="pill" href="#menu1">History Jawaban</a>
-            </li>
-        </ul>
+    <!-- ini navbar -->
+  <div class="nav">
+    <nav>
+      <a class="active" href="#">Upload Jawaban</a>
+      <a href="#conKeterangan" style="margin-left: 3.5px">History Jawaban</a>
+    </nav>
+  </div>
+  <!-- ini dropdown -->
+  <div class=" dropdown">
+    <button class="btn btn-secondary dropdown-toggle drpdwn" type="button" id="dropdownProfile" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="border-radius: 30px; height: 45px;width: 226px;">
+      Anasya Wulandari<span> <i class="fas fa-user"></i></span>
+    </button>
+    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    <a class="dropdown-item" href="#">Profil</a>
+    <a class="dropdown-item" href="#">Logout</a>
     </div>
-    <div class="dropdown">
-            <button class="btn btn-secondary dropdown-toggle drpdwn" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Anasya Wulandari<i class="fas fa-user"></i>
-            </button>
-            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                <a class="dropdown-item" href="#">Profil</a>
-                <a class="dropdown-item" href="#">Logout</a>
-            </div>
-    </div>
+  </div>
 </div>
     <div class="row">
         <div class="box">
